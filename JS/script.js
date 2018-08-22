2f730fc0503f4969878a7990dfedc128d2a9dc5a
Ernest <arnesto559@gmail.com>
2018-08-22 10:12:48 +0300

    Will store the question variables.

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,9 @@
 function check() {
-
+  var question1 = document.quiz.question1.value;
+    var question2 = document.quiz.question2.value;
+    var question3 = document.quiz.question3.value;
+    var question4 = document.quiz.question4.value;
+    var result = 0;
 
 
 

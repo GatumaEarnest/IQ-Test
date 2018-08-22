3f81374c4a00b310455aaa9d873fab4d6c990b45
Ernest <arnesto559@gmail.com>
2018-08-22 10:21:09 +0300

    Will add the Second question work out.

--- JS/script.js
+++ JS/script.js
@@ -4,11 +4,14 @@ function check() {
     var question3 = document.quiz.question3.value;
     var question4 = document.quiz.question4.value;
     var result = 0;
-
-    //Question 1
-      if (question1 == "Yes alot!"){result+=85}
-      else if (question1 = "Am not sure."){result+=75}
-      else if (question1 =="Sometimes." )  {result*=55;}
+  //Question 1
+    if (question1 == "Yes alot!"){result+=85}
+    else if (question1 = "Am not sure."){result+=75}
+    else if (question1 =="Sometimes." )  {result*=55;}
+    //Question 2
+    if (question2 == "Of course!"){result1+=60}
+    else if (question2 == "Nah.The cat comes first."){result2+=200}
+    else if (question2 =="I don't like pets" )  {result2+=30;}
 
 
 
